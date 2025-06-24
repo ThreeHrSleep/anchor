a04c01f498a18b232dddb92d3583d186a78d5c64
ThreeHrSleep <threehrsleep@gmail.com>
2025-06-24 14:55:12 +0530

    make random_key() public

--- anchor/common/bls_lagrange/src/lib.rs
+++ anchor/common/bls_lagrange/src/lib.rs
@@ -31,7 +31,7 @@ pub fn split(
 }
 
 #[cfg(any(feature = "blst", test))]
-pub(crate) fn random_key(rng: &mut (impl CryptoRng + Rng)) -> Result<SecretKey, Error> {
+pub fn random_key(rng: &mut (impl CryptoRng + Rng)) -> Result<SecretKey, Error> {
     let ikm = zeroize::Zeroizing::new(rng.r#gen::<[u8; 32]>());
     let sk =
         ::blst::min_pk::SecretKey::key_gen(ikm.as_ref(), &[]).map_err(|_| Error::InternalError)?;
